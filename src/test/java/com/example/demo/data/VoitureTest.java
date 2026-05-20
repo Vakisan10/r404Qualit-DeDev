213da81b64850d2592df8d84c06b7be9fca13c76
MATHIGNANASINGAM Vakisan <12306758@r20312.iutv.univ-paris13.fr>
2026-05-20 11:17:26 +0200

    mon test

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -9,13 +9,20 @@ public class VoitureTest {
 
     @BeforeAll
     void creerVoiture(){
-        Voiture voiture= new Voiture("Volkswagen",3800)
+        Voiture voiture1= new Voiture("Volkswagen",3800);
+        voiture1.setId()==1;
+        Voiture voiture2= new Voiture("Ferrari",18000);
+        voiture2.setId()==2;
         }
     @Test
     void TestVoiture(){
-        Assert.istrue(voiture.getPrix() ==3800,"Doit être 3800")
-        Assert.isTrue(voiture.getMarque().equals("Volkswagen"),"Doit être Volkswagen")
-
+        Assert.isTrue(voiture1.getPrix() ==3800,"Doit être 3800");
+        Assert.isTrue(voiture1.getMarque().equals("Volkswagen"),"Doit être Volkswagen");
+        Assert.isTrue(voiture1.getId().equals(1));
+        Assert.isTrue(voiture2.getPrix() ==18000,"Doit être 18000");
+        Assert.isTrue(voiture2.getMarque()== "Ferrari","Doit être Volkswagen");
+        Assert.isFalse(voiture1.getId().equals(1));
+        
     }
 
 }
